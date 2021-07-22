7b9a97f7f6aa4123143ffd3400c030cf3f6ed62d
lukaszplk <86591130+lukaszplk@users.noreply.github.com>
2021-07-22 16:11:22 +0200

    Update main.cpp
    Add 2 comments

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include "matrix.cpp"
 
 int main(){
+    //Declare the dimensions of the matrix
     int n = 3, m = 4;
-
+    
+    //Enter the values in the matrix, u'll see ur matrix in console after run program to make sure that's proper
     float tab[] = {1,2,-1,5,3,4,1,9,2,-2,3,-1};
 
     Matrix M(n,m,tab), G(n,m);
